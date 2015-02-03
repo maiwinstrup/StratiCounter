b40a20c163052c2982ba12c45047ade41d6faa1e
mai <mai@gfy.ku.dk>
2015-02-02 18:44:39 -0800

    Correctly solved bug that sometimes caused a missing layer in last batch

--- straticounter.m
+++ straticounter.m
@@ -32,6 +32,16 @@ function straticounter(sett_icecore)
 clc; close all;
 releasedate = '02-02-2015';
 
+% Copyright (C) 2015 Mai Winstrup
+% This program is free software; you can redistribute it and/or modify it 
+% under the terms of the GNU General Public License as published by the 
+% Free Software Foundation; either version 2 of the License, or (at your 
+% option) any later version.
+%    This program is distributed in the hope that it will be useful, but 
+% WITHOUT ANY WARRANTY; without even the implied warranty of 
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General 
+% Public License for more details.
+
 %% Check that settings file exist:
 if ~exist(['./Settings/' sett_icecore '.m'],'file')
     disp('Settings file unknown, please correct')
